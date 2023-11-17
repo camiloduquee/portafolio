18f47f7013d090a5711238bf9db7538e269e55f9
camiloduquee <camiloduquee@gmail.com>
2023-11-17 06:09:17 -0400

    module bug version

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.alias['entities'] = false;
+      }
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+
