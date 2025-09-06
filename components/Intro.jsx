41e26627f9fc8107412310f1ed543f3545feb7a3
Héctor Duque <camiloduquee@gmail.com>
2025-09-05 21:11:12 -0400

    Update Intro.jsx

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -62,7 +62,7 @@ export default function Intro() {
             </Link>
             <Link
               className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10"
-              href="/Hector-Gomez.pdf"
+              href="/Hector-Duque.pdf"
               target="_blank"
               rel="noopener noreferrer"
             >
